f09e2d6d4d93bbf56f80e0e69eb821dc5ca47526
Filip Crisan <filcrisan@gmail.com>
2022-11-12 21:15:35 +0200

    feat: add fa menu

---
+++ lab/FiniteAutomaton.cs
@@ -0,0 +1,92 @@
+namespace lab;
+
+public class FiniteAutomaton
+{
+    private readonly HashSet<string> _states;
+    private readonly HashSet<string> _alphabet;
+    private readonly string _initialState;
+    private readonly HashSet<string> _finalStates;
+    private readonly Dictionary<(string, string), HashSet<string>> _transitions;
+
+    public FiniteAutomaton()
+    {
+        var lines = File.ReadAllLines("../../../res/FA.in");
+
+        _states = new HashSet<string>(lines[0].Split(" "));
+        _alphabet = new HashSet<string>(lines[1].Split(" "));
+        _initialState = lines[2];
+        _finalStates = new HashSet<string>(lines[3].Split(" "));
+
+        _transitions = new Dictionary<(string, string), HashSet<string>>();
+        for (var i = 4; i < lines.Length; i++)
+        {
+            var source = lines[i].Split(" ")[0];
+            var edge = lines[i].Split(" ")[1];
+            var target = lines[i].Split(" ")[2];
+            
+            if (!_states.Contains(source) || !_states.Contains(target) || !_alphabet.Contains(edge)) continue;
+
+            var key = (source, edge);
+            if (!_transitions.ContainsKey(key))
+            {
+                var targetStates = new HashSet<string> { target };
+                _transitions.Add(key, targetStates);
+            }
+            else
+            {
+                _transitions.GetValueOrDefault(key)!.Add(target);
+            }
+        }
+    }
+
+    public string StatesToString() => $"States: {string.Join(", ", _states)}";
+
+    public string AlphabetToString() => $"Alphabet: {string.Join(", ", _alphabet)}";
+
+    public string InitialStateToString() => $"Initial state: {_initialState}";
+
+    public string FinalStatesToString() => $"Final states: {string.Join(", ", _finalStates)}";
+
+    public string TransitionsToString()
+    {
+        var result = "Transitions:\n";
+
+        foreach (var ((source, edge), targets) in _transitions)
+        {
+            result += $"Source: {source}, edge: {edge}, targets: {string.Join(", ", targets)}\n";
+        }
+        
+        return result;
+    }
+
+    public bool IsDeterministic()
+    {
+        foreach (var (_, targets) in _transitions)
+        {
+            if (targets.Count > 1)
+            {
+                return false;
+            } 
+        }
+
+        return true;
+    }
+
+    public bool IsSequenceValid(string sequence)
+    {
+        if (sequence.Length == 0)
+        {
+            return _finalStates.Contains(_initialState);
+        }
+
+        var currentState = _initialState;
+        foreach (var key in sequence.Select(character => (currentState, character.ToString())))
+        {
+            if (!_transitions.ContainsKey(key)) return false;
+
+            currentState = _transitions.GetValueOrDefault(key)!.First();
+        }
+
+        return _finalStates.Contains(currentState);
+    }
+}
--- lab/Program.cs
+++ lab/Program.cs
@@ -1,11 +1,82 @@
 ﻿using lab;
 
-try
+void Menu()
 {
-    var scanner = new Scanner();
-    scanner.Scan();
+    Console.WriteLine("1. Scan");
+    Console.WriteLine("2. Show FA menu");
+    Console.Write(">>>>>> ");
+
+    var option = Convert.ToInt32(Console.ReadLine());
+
+    switch (option)
+    {
+        case 1: Scan();
+            break;
+        case 2: FaMenu();
+            break;
+    }
 }
-catch (LexicalException le)
+
+int GetFaMenuOption()
 {
-    Console.WriteLine(le.Message + ". Line: " + le.Line);
+    Console.WriteLine();
+    Console.WriteLine("1. All states");
+    Console.WriteLine("2. Alphabet");
+    Console.WriteLine("3. Transitions");
+    Console.WriteLine("4. Initial state");
+    Console.WriteLine("5. Final states");
+    Console.WriteLine("6. Check you sequence");
+    Console.Write(">>>>>> ");
+    return Convert.ToInt32(Console.ReadLine());
 }
+
+void FaMenu()
+{
+    var finiteAutomaton = new FiniteAutomaton();
+    int option;
+    do
+    {
+        option = GetFaMenuOption();
+
+        switch (option)
+        {
+            case 1: Console.WriteLine(finiteAutomaton.StatesToString());
+                break;
+            case 2: Console.WriteLine(finiteAutomaton.AlphabetToString());
+                break;
+            case 3: Console.WriteLine(finiteAutomaton.TransitionsToString());
+                break;
+            case 4: Console.WriteLine(finiteAutomaton.InitialStateToString());
+                break;
+            case 5: Console.WriteLine(finiteAutomaton.FinalStatesToString());
+                break;
+            case 6:
+            {
+                if (!finiteAutomaton.IsDeterministic())
+                {
+                    Console.WriteLine("Non deterministic!");
+                    continue;
+                }
+                Console.Write("Enter your sequence >>>>>> ");
+                var sequence = Console.ReadLine() ?? string.Empty;
+                Console.WriteLine(finiteAutomaton.IsSequenceValid(sequence) ? "Valid" : "Invalid");
+            };
+                break;
+        }
+    } while (option != 0);
+}
+
+void Scan()
+{
+    try
+    {
+        var scanner = new Scanner();
+        scanner.Scan();
+    }
+    catch (LexicalException le)
+    {
+        Console.WriteLine(le.Message + ". Line: " + le.Line);
+    }
+}
+
+Menu();
